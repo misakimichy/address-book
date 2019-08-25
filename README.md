4aa089815a7ca8c3780d2bbc6c224bbe63d15849
Misaki Koonce <misaki.koonce@gmail.com>
2019-08-25 15:36:29 -0700

    Initialize the project.

---
+++ README.md
@@ -0,0 +1,34 @@
+# Address Book
+
+#### _Given a task to build a Address Book Webpage, 8/25/2019_
+#### _**08/25/19: Misaki Koonce**_
+
+## Description
+Build a Address Book (using HTML, CSS and JavaScript)
+
+### Website should have:
+
+An address book application using constructors and prototypes.
+
+
+## Setup/Installation Requirements
+
+* Clone this repo
+* Navigate to the top level of the cloned directory.
+* Then, open your preferred web browser.
+
+## Known Bugs
+
+* Work in progress.
+
+## Support and contact details
+
+ misaki.koonce@gmail.com
+
+## Technologies Used
+
+_Git Github  HTML Terminal CSS and JavaScript
+
+### License
+
+Copyright (c) 2019 under the MIT License
---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+function AddressBook() {
+    this.contacts = [],
+    this.currentId = 0
+}
+
+AddressBook.prototype.addContact = function(contact) {
+    contact.id = this.assignId();
+    this.contacts.push(contact);
+}
+
+AddressBook.prototype.assignId = function() {
+    this.currentId++;
+    return this.currentId;
+}
+
+AddressBook.prototype.findContact = function(id) {
+    for (let i = 0; i < this.contacts.length; i++) {
+        if(this.contacts[i]){
+            if(this.contacts[i].id == id) {
+                return this.contacts[i];
+            }
+        }
+    };
+    return false;
+}
+
+AddressBook.prototype.deleteContact = function(id) {
+    for (let i = 0; i < this.contacts.length; i++) {
+        if(this.contacts[i]){
+            if(this.contacts[i].id == id) {
+                delete this.contacts[i];
+                return true;
+            }
+        }
+    };
+    return false;
+}
+
+function Contact (firstName, lastName, phoneNumber) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+}
+
+Contact.prototype.fullName = function() {
+    return this.firstName + this.lastName;
+}
+
+let addressBook = new AddressBook();
+let contact = new Contact("Ada", "Lovelace", "503-555-0100");
+let contact2 = new Contact("Grace", "Hopper", "503-555-0199");
+addressBook.addContact(contact);
+addressBook.addContact(contact2);
